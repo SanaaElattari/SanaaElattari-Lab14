ba7a45a96fe37dfdb3210d250d33956d72e48400
SanaaElattari <sanaaelattari2006@gmail.com>
2025-11-10 14:25:14 -0500

    skeleton of both classes

--- Client.java
+++ Client.java
@@ -1,3 +1,41 @@
+import java.io.BufferedReader;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+
 public class Client {
     
+    private String host;
+    private int port;
+    private Socket socket;
+    BufferedReader in; 
+
+
+    public Client(String host, int port) throws IOException {
+        this.host = host;
+        this.port = port;
+        this.socket = new Socket(host, port);
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public void handshake() throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("12345");
+    }
+
+    public String request(String num)
+    {
+        return num; // for now 
+    }
+
+    public void disconnect() throws IOException {
+        socket.close();
+    }
+
+
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,27 @@
+import java.net.Socket;
+
 public class Server {
-    
+    private int port;
+    private ServerSocket serverSocket;
+  //private ArrayList<ClientHandler> clients;
+    //private connectedTimes ArrayList<LocalDateTime>;
+
+    public Server(int port) throws IOException {
+        this.port = port;
+        this.serverSocket = new ServerSocket(port);
+        //this.clients = new ArrayList<ClientHandler>();
+        //this.connectedTimes = new ArrayList<LocalDateTime>();
+    }
+
+    public void serve (int numClients)
+    {
+
+    }
+
+    public disconnect() throws IOException {
+        serverSocket.close();
+    }
+
+    //client handler class
+
 }
